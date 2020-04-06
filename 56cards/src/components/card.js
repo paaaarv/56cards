18d1cf20bc682b69ed192cb5d0ae933f8b35c6f0
paaaarv <pkumar@trinity.edu>
2020-04-05 20:26:23 -0500

    finished creating basic card

--- 56cards/src/components/card.js
+++ 56cards/src/components/card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/card.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 
 export default class Card extends React.Component{
@@ -10,7 +11,9 @@ export default class Card extends React.Component{
     return(
       <div className='border'>
       <p className='top-left'>k</p>
-      <br/><br/>
+      <div className="icon">
+      <FontAwesomeIcon icon={faHeart} size="lg" className="icon"/>
+      </div>
       <p className='bottom-right'>k</p>
       </div>
     )
