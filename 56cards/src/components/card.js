8088da3272ad176ed752b996ae8a3fc6fb6f8007
paaaarv <pkumar@trinity.edu>
2020-04-05 19:02:18 -0500

    edited card component to have values on corners

---
+++ 56cards/src/components/card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../assets/card.css'
+
+
+
+export default class Card extends React.Component{
+
+
+  render(){
+    return(
+      <div className='border'>
+      <p className='top-left'>k</p>
+      <br/><br/>
+      <p className='bottom-right'>k</p>
+      </div>
+    )
+  }
+
+
+
+
+
+}
