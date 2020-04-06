b9a57627cb06ade642915435b0a761cfdc75a44c
paaaarv <pkumar@trinity.edu>
2020-04-06 15:45:22 -0500

    added a deck component, state with values of cards

---
+++ 56cards/src/components/deck.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+
+
+
+export default class Deck extends React.Component{
+
+
+  constructor(props){
+    super(props)
+    this.state = {
+      'K': ['hearts', 'diamonds', 'clubs', 'spades'],
+      'Q': ['hearts', 'diamonds', 'clubs', 'spades'],
+      'J': ['hearts', 'diamonds', 'clubs', 'spades'],
+      '10': ['hearts', 'diamonds', 'clubs', 'spades'],
+      '9':['hearts', 'diamonds', 'clubs', 'spades'],
+      '8':['hearts', 'diamonds', 'clubs', 'spades'],
+      '7':['hearts', 'diamonds', 'clubs', 'spades']
+    }
+
+  }
+
+
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+
+}
