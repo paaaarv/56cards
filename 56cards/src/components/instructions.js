27933ac8eff78ff9ab0a32927f0635c7612c8e38
paaaarv <pkumar@trinity.edu>
2020-04-13 16:16:53 -0500

    added next page and previous page buttons to instructions

--- 56cards/src/components/instructions.js
+++ 56cards/src/components/instructions.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import '../assets/instructions.css'
-
+import Button from 'react-bootstrap/button'
 export default class Instructions extends React.Component{
-
-
 
 
   render(){
 
-    const array = []
+    const array = ['this is how you play', 'we put the cards in the pile', 'and then you deal them', 'and then everyone has cards', 'you play them one at a time', 'then someone wins']
   return(
     <div>
     <h3> how to play: </h3>
@@ -17,8 +15,9 @@ export default class Instructions extends React.Component{
     <p className='paragraphs'>
 
     </p>
-    </div> 
-    <button> next page -> </button>
+    </div>
+    <Button size='sm' variant='outline-info' as='input' type='button' value="Prev Page"/>
+    <Button size='sm' variant='outline-info' as='input' type='button' value="Next Page"/>
     </div>
 
   )}
