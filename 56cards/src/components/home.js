f93684cae652833aaee0f6841486fa41ce034d17
paaaarv <parvathikumar92@gmail.com>
2020-04-23 17:25:22 -0500

    directions showing up on page

--- 56cards/src/components/home.js
+++ 56cards/src/components/home.js
@@ -4,13 +4,23 @@ import Instructions from './instructions.js'
 
 
 export default class Home extends React.Component{
+  constructor(props){
+    super(props)
+    this.i= 0
+    this.array = ['this is how you play', 'we put the cards in the pile', 'and then you deal them', 'and then everyone has cards', 'you play them one at a time', 'then someone wins']
+    this.page=this.array[0]
+  }
 
+  handleClick = () =>{
+    ++this.i;
+    this.page = this.array[this.i]
+  }
 
   render(){
     return(
       <div>
       <h1> 56 </h1>
-      <Instructions/> 
+      <Instructions page={this.page}/>
       </div>
     )
   }
--- 56cards/src/components/instructions.js
+++ 56cards/src/components/instructions.js
@@ -3,13 +3,9 @@ import React from 'react';
 import '../assets/instructions.css'
 import Button from 'react-bootstrap/button'
 export default class Instructions extends React.Component{
-      constructor(props){
-        super(props)
-        this.i= 0
-        this.array = ['this is how you play', 'we put the cards in the pile', 'and then you deal them', 'and then everyone has cards', 'you play them one at a time', 'then someone wins']
 
-      }
 
+ 
    onClick =(e)=>{
     if(e.target.value === "Next Page"){
       console.log('hi')
@@ -23,7 +19,7 @@ export default class Instructions extends React.Component{
     <h3> how to play: </h3> <br/>
     <div className='pcontainer'>
     <p className='paragraphs'>
-
+        {this.props.page}
     </p>
     </div> <br/>
     <Button onClick={this.onClick} size='sm' variant='outline-info' as='input' type='button' value="Prev Page"/>
