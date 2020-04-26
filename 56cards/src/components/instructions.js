413017b8ffac8e44ea174be32e0060e3ba68c513
paaaarv <pkumar@trinity.edu>
2020-04-26 13:41:00 -0500

    added to instruction

--- 56cards/src/components/instructions.js
+++ 56cards/src/components/instructions.js
@@ -5,14 +5,22 @@ import Button from 'react-bootstrap/button'
 export default class Instructions extends React.Component{
       constructor(props){
         super(props)
-        this.i= 0
-        this.array = ['this is how you play', 'we put the cards in the pile', 'and then you deal them', 'and then everyone has cards', 'you play them one at a time', 'then someone wins']
+        this.i = 1
+        this.state = {page:'this is how you play',
+        2: 'we put the cards in the pile',
+        3: 'and then you deal them',
+        4: 'and then everyone has cards',
+        5: 'you play them one at a time',
+        6: 'then someone wins'}
 
       }
 
    onClick =(e)=>{
     if(e.target.value === "Next Page"){
-      console.log('hi')
+      ++this.i
+      this.setState({
+        page: this.array[i]
+      })
     }
 
   }
@@ -23,7 +31,7 @@ export default class Instructions extends React.Component{
     <h3> how to play: </h3> <br/>
     <div className='pcontainer'>
     <p className='paragraphs'>
-
+    {this.state.page}
     </p>
     </div> <br/>
     <Button onClick={this.onClick} size='sm' variant='outline-info' as='input' type='button' value="Prev Page"/>
