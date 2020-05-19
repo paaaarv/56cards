be4dd63c1da668011c9d934f7649dd04a86eb0f8
paaaarv <parvathikumar92@gmail.com>
2020-05-19 12:38:02 -0500

    login page added

--- 56cards/src/App.js
+++ 56cards/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Home from './components/home.js'
 import Card from './components/card.js'
 import Deck from './components/deck.js'
+import Login from './components/login.js'
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Home/>
         <Deck/>
         <Card/>
+        <Login/>
     </div>
   );
 }
---
+++ 56cards/src/components/login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Form from 'react-bootstrap/form'
+
+
+
+
+
+export default class Login extends React.Component{
+
+
+  render(){
+    return(
+      <Form>
+      <Form.Label> Hi </Form.Label>
+      </Form>
+
+
+    )
+  }
+
+}
